3336954e7395a3579212dcd2122946df1852077f
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-09 13:20:29 +0200

    fix 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,7 +8,7 @@ Console.Write("jar e kedvezmeny");
 bool kedvezmeny = bool.Parse(Console.ReadLine());
 if (kedvezmeny == true)
 {
-if (termekar >= 10000) Console.WriteLine("a termék ára: " + termekar * termemeny * 0.9);
+if ((termekar*termemeny) >= 10000) Console.WriteLine("a termék ára: " + termekar * termemeny * 0.9);
 else Console.WriteLine("a termék ára: " + termekar * termemeny);
 }
 else
