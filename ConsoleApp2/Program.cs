964707200f46849495387e76a1ce0834b4a5da77
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-09 13:17:42 +0200

    teszt1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,3 +4,13 @@ Console.Write("add meg a termék egységárát");
 int termekar = int.Parse(Console.ReadLine());
 Console.Write("add meg a termék vásárolt mennyiségét");
 int termemeny = int.Parse(Console.ReadLine());
+Console.Write("jar e kedvezmeny");
+bool kedvezmeny = bool.Parse(Console.ReadLine());
+if (kedvezmeny == true)
+{if (termekar >= 10000) Console.WriteLine("a termék ára: " + termekar * termemeny * 0.9);
+else Console.WriteLine("a termék ára: " + termekar * termemeny);
+}
+else
+{
+    Console.WriteLine("a termék ára: " + termekar * termemeny);
+}
