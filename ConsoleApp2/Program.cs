ece04910f4eb7493dd1501d3e89cf27c40f43241
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-09 13:13:24 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("add meg a termék nevét");
+string termekNev = Console.ReadLine();
+Console.Write("add meg a termék egységárát");
+int termekar = int.Parse(Console.ReadLine());
+Console.Write("add meg a termék vásárolt mennyiségét");
+int termemeny = int.Parse(Console.ReadLine());
